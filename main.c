29fd47a7907e4e35613af1bb3592d6b266683b19
Manu <manuelr0ficial@gmail.com>
2026-09-08 23:36:35 +0200

    fix error lines in main.c

--- main.c
+++ main.c
@@ -27,10 +27,6 @@ int re_tolower(char string[]){
 int main(int argc, char **argv){
     
     int UME = 0; //Unknown month error;
-    int TPE = 0; //Timestamp parsing error;
-    
-
-
 
     time_t current_date = time(NULL); //Primero obtenemos la fecha en segundos del Epoch time gracias a time(NULL);
 
@@ -43,7 +39,6 @@ int main(int argc, char **argv){
         t->tm_mon + 1,
         t->tm_year + 1900
     };
-    
 
    Date today = {
 
@@ -54,7 +49,6 @@ int main(int argc, char **argv){
 
    }; 
 
-
     /*Después copiamos los datos correspondientes del dia mes y año obtenidos con el puntero *t al struct
     (Date) today para saber la fecha de ese día y poner el encabezado del mes y esas cosas*/
 
@@ -63,35 +57,21 @@ int main(int argc, char **argv){
 
     static const char *mn_short[12] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
 
-    
-  
     for (int i = 0; mn[i] != NULL && mn_short[i] != NULL && argc >= 2; i++){
         re_tolower(argv[1]);
         if (strcmp(argv[1], mn[i]) == 0){ date.month = i + 1; UME = 0; break;}
         if (strcmp(argv[1], mn_short[i]) == 0){mn_short[i] = mn[i]; date.month = i + 1; UME = 0; break;}
-        else UME = 1;
-        
-        
-        
-    
+        else UME = 1; 
     }
-
-    
-        
 
     if (argc >= 3 && argv[2] != NULL){
         int i = atoi(argv[2]);
-        printf("%d\n", i);
-        if (i > 9999 || i <= 0) {fprintf(stderr, "HF-CAL: illegal year value '%s': Numerical result out of range", argv[2]); return 1;}
-        else if (i == 0) {fprintf(stderr, "HF-Cal: illegal year value: use positive integers\n"); return 1;}
+        if (i > 9999 || i <= 0) {fprintf(stderr, "hf-cal: illegal year value '%s': Numerical result out of range", argv[2]); return 1;}
+        else if (i == 0) {fprintf(stderr, "hf-cal: illegal year value: use positive integers\n"); return 1;}
         date.year = i;
-     
-
     }
 
-     
-    
-    if (UME) {fprintf(stderr, "HF-Cal: failed to parse timestamp or unknown month name: %s\n", argv[1]); return 1;}
+    if (UME) {fprintf(stderr, "hf-cal: failed to parse timestamp or unknown month name: %s\n", argv[1]); return 1;}
 
     int DIM = days_in_month(date.month, date.year); // DIM -> Days In Month
     int weekday = get_weekday(1, date.month, date.year);
