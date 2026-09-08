e768fe4dc4a23ecdec5421f40dcc4d58480a3d3b
Manu <manuelr0ficial@gmail.com>
2026-09-08 13:07:05 +0200

    added support for months abbreviation input

--- main.c
+++ main.c
@@ -4,6 +4,28 @@
 #include <stdlib.h>
 #include "calendar.h"
 
+
+
+int re_tolower(char string[]){
+
+    //char name[] = "KIRITO"
+    //re_tolower(name);
+    //printf("%s\n", name); -> kirito
+    
+    int i = 0;
+    int j = (int) string[i];
+    
+    while (string[i] != '\0'){
+
+    int j = (int) string[i]; 
+    if (string[i] >= 65 && string[i] <= 90){ j += 32;}
+    string[i] = (char)j;
+    i++;
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv){
     
     time_t current_date = time(NULL); //Primero obtenemos la fecha en segundos del Epoch time gracias a time(NULL);
@@ -32,16 +54,19 @@ int main(int argc, char **argv){
     /*Después copiamos los datos correspondientes del dia mes y año obtenidos con el puntero *t al struct
     (Date) today para saber la fecha de ese día y poner el encabezado del mes y esas cosas*/
 
-    static const char *mn[12] = {"january", "february", "march", "april", "may", "june", "july", // mn -> months name
+    static const char *mn[12] = {"january", "february", "march", "april", "may", "june", "july",  // mn -> months name
                         "august", "september", "october", "november", "december"};
 
-    static const char *wrong_mn[12] = {"January", "February", "March", "April", "May", "June", "July", 
-                                       "August", "September", "October", "November", "December"};
+    static const char *mn_short[12] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
 
+    
   
-    for (int i = 0; mn[i] != NULL && argc >= 2; i++){
+    for (int i = 0; mn[i] != NULL && mn_short[i] != NULL && argc >= 2; i++){
+        //printf("%s\n", mn_short[i]); 
+        re_tolower(argv[1]);
         if (strcmp(argv[1], mn[i]) == 0) date.month = i + 1;
-        else if (strcmp(argv[1], wrong_mn[i]) == 0) break;  
+        else if (strcmp(argv[1], mn_short[i]) == 0){mn_short[i] = mn[i]; date.month = i + 1;}
+     
     }
 
     if (argc >= 3 && argv[2] != NULL){
